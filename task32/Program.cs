5b9c714cba9bbebb9c40763196995a7d4bab75bd
Margarita <margarita27011991@gmail.com>
2022-08-20 17:35:28 +0300

    замена положительных элементов массива на соответствующие отрицательные

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,8 +1,6 @@
 ﻿// Задача 32: Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот. [-4, -8, 8, 2] -> [4, 8, -8, -2]
 Console.Clear();
-int[] array = new int[4]; // можно менять количество элементов
-int sumPositive = 0;
-int sumNegative = 0;
+int[] array = new int[7]; // можно менять количество элементов
 Random rand = new Random();
 for(int i = 0; i < array.Length; i++)
 {
@@ -11,13 +9,14 @@ for(int i = 0; i < array.Length; i++)
 Console.WriteLine('['+string.Join(", ", array) + ']');
 for(int i = 0; i < array.Length; i++)
 {
-    if(array[i]> 0)
-    {
-        sumPositive+= array[i];
-    }
-    else
-    {
-        sumNegative+= array[i];
-    }
+    array[i] = array[i] * -1;
+    // if(array[i] != 0)
+    // {
+    //     array[i] = array[i] * -1;
+    // }
+    // else
+    // {
+    //     array[i] = array[i];
+    // }
 }
-Console.WriteLine($"сумма положительных чисел равна {sumPositive} сумма отрицательных равна {sumNegative} ");
+Console.WriteLine('['+string.Join(", ", array) + ']');
